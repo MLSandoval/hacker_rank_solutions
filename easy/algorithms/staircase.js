0f4ea63890bdc662945fdcc3e90947d1b9a0ec34
mlsandoval <michaelsandoval8@gmail.com>
2020-08-30 00:20:37 -0700

    staircase started

---
+++ easy/algorithms/staircase.js
@@ -0,0 +1,10 @@
+function staircase(n) {
+  const hash = '#'
+  const space = ' '
+
+  for(let i = 1; i <= n; i++){
+    console.log(`${space.repeat(n - i)}${hash.repeat(i)}`)
+  }
+}
+
+staircase(8)
