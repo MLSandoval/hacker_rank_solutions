87963b6202c504549ec149e794147b0ea47f846f
mlsandoval <michaelsandoval8@gmail.com>
2020-08-30 02:20:38 -0700

    mini max sums started, built shell sort with help but want to rebuild it from scratch no help tomorrow

---
+++ easy/algorithms/mini-max-sums.js
@@ -0,0 +1,29 @@
+var shellSorty = (arr) => {
+  var increment = arr.length / 2
+  while(increment > 0){
+    for(var i = increment; i < arr.length; i++){
+      var j = i
+      var temp = arr[i]
+
+      while(j >= increment && arr[j-increment] > temp){
+        arr[j] = arr[j-increment]
+        j = j - increment
+      }
+
+      arr[j] = temp
+    }
+
+      if(increment == 2){
+        increment = 1
+      }else{
+        increment = parseInt(increment*5 / 11)
+      }
+    
+  }
+  return arr
+}
+
+  console.log(shellSorty([2, 4, 1, 2, 0, 9, 1, 8, 7, 2, 7, 5]))
+
+
+
