6895c037d92865b6cde53855b82453f48366acb9
mlsandoval <michaelsandoval8@gmail.com>
2020-08-31 20:50:19 +0000

    completed mini-max-sum

--- easy/algorithms/mini-max-sums.js
+++ easy/algorithms/mini-max-sums.js
@@ -1,29 +1,37 @@
-var shellSorty = (arr) => {
-  var increment = arr.length / 2
-  while(increment > 0){
-    for(var i = increment; i < arr.length; i++){
-      var j = i
-      var temp = arr[i]
+const miniMaxSum = (arr) => {  
+  const shellSorty = () => {
+    let gap = (arr.length / 2).toFixed(0)
+    while(gap > 0){
+      for(let i = gap; i < arr.length; i++){
+        let increment = i
+        let temp = arr[i]
 
-      while(j >= increment && arr[j-increment] > temp){
-        arr[j] = arr[j-increment]
-        j = j - increment
+        while(increment >= gap && arr[increment-gap] > temp){
+          arr[increment] = arr[increment-gap]
+          increment = increment - gap
+        }
+
+        arr[increment] = temp
       }
 
-      arr[j] = temp
-    }
-
-      if(increment == 2){
-        increment = 1
+      if(gap == 2){
+        gap = 1
       }else{
-        increment = parseInt(increment*5 / 11)
+        gap = parseInt(gap*5 / 11)
       }
-    
+    }
+    return arr
   }
-  return arr
-}
 
-  console.log(shellSorty([2, 4, 1, 2, 0, 9, 1, 8, 7, 2, 7, 5]))
+  let sortedArr = shellSorty(arr)
+
+  let smallestSum = sortedArr.slice(0, 4).reduce((cur, acc)=>acc+cur)
+  let largestSum = sortedArr.slice(-4).reduce((cur, acc)=>acc+cur)
+
+  console.log(smallestSum + ' ' + largestSum)
+}
+  
+miniMaxSum([5, 4, 3, 1, 2])
 
 
 
