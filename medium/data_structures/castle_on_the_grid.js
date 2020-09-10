7a636e4ec636d4e50c0107b209ad502977e97f75
mlsandoval <michaelsandoval8@gmail.com>
2020-09-10 03:37:39 +0000

    started the first attempt at solution

--- medium/data_structures/castle_on_the_grid.js
+++ medium/data_structures/castle_on_the_grid.js
@@ -1,4 +1,23 @@
 function minimumMoves(grid, startX, startY, goalX, goalY) {
+  let xDiff, yDiff
 
+  for(let i = startX; i <= goalX; i++){
+    for(let j = startY; j <= goalY; j++){
+      
+    }
+  }
+
+
+
+  function findBlocks(){}
 
 }
+
+
+let sq = [
+  '.X.',
+  '.X.',
+  '...',
+]
+
+console.log(minimumMoves(sq, 0, 0, 0, 2))
